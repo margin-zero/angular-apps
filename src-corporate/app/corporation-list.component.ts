0b7fa5c2057090ae96adfb0aafaae38896a16e17
Tomek <margin-zero@atcplanet.com>
2017-10-07 22:21:08 +0200

    corporation.list - display names of county and city insted of their ids

--- src-corporate/app/corporation-list.component.ts
+++ src-corporate/app/corporation-list.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Corporation } from './corporation';
+import { City } from './city';
+import { Country } from './country';
 
 import { CorporationService } from './corporation.service';
+import { CityService } from './city.service';
+import { CountryService } from './country.service';
 
 import { Router } from '@angular/router';
 
@@ -15,22 +19,48 @@ import { Router } from '@angular/router';
 export class CorporationListComponent implements OnInit {
 
   corporations: Corporation[];
+  cities: City[];
+  countries: Country[];
   selectedCorporation: Corporation;
 
   constructor(
     private corporationService: CorporationService,
+    private cityService: CityService,
+    private countryService: CountryService,
     private router: Router) { }
 
   getCorporations(): void {
     this.corporationService.getCorporations().then(corporations => this.corporations = corporations);
   }
 
+  getCities(): void {
+    this.cityService.getCities().then(cities => this.cities = cities);
+  }
+
+  getCountries(): void {
+    this.countryService.getCountries().then(countries => this.countries = countries);
+  }
+
   ngOnInit(): void {
     this.getCorporations();
+    this.getCities();
+    this.getCountries();
   }
 
   onSelect( corporation: Corporation ){
     this.selectedCorporation = corporation;
     this.router.navigate(['./corporation-edit/'+this.selectedCorporation.id])
+  }
+
+  getCityName(city_id) {
+    return function(element) {
+      return element.id == city_id;
+    }
+  }
+
+  getCountryName(country_id) {
+    return function(element) {
+      return element.id == country_id;
+    }
   }
 }
